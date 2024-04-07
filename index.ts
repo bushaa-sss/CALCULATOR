96b52322647a1d2ea99e73a1a2569a51a539aa07
Bushra Saleh <117832612+bushaa-sss@users.noreply.github.com>
2024-04-08 03:20:15 +0500

    created simple calculator

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+//[]=array, {}= object
+const answer = await inquirer.prompt([
+    {
+        message: "enter first number",
+        type: "number",
+        name: "fnum",
+    },
+    { message: "enter second number",
+        type: "number",
+        name: "snum" },
+    {
+        message: "select any operator",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "DIVISION", "SUBTRACTION", "MULTLPICATION"],
+    },
+]);
+if (answer.operator === "ADDITION") {
+    console.log(answer.fnum + answer.snum);
+}
+else if (answer.operator === "SUBTRACTION") {
+    console.log(answer.fnum - answer.snum);
+}
+else if (answer.operator === "MULTLPICATION") {
+    console.log(answer.fnum * answer.snum);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.fnum / answer.snum);
+}
+else {
+    console.log("enter valid operator");
+}
